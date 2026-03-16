d6c560830ffd3832145530029942c35b87f1aaab
chetan1883 <chetanlandge1883@gmail.com>
2026-03-16 23:06:16 +0530

    fix duplicate API declaration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,4 @@
-//import axios from "axios";
-import API from "../api/axios";   // adjust path if needed
+import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://smart-waste-backend-9qy4.onrender.com"
